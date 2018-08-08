1b2150e75b3688536f130d241b125816fb5a736d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 17:03:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |name, index|
+    "#{index + 1} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
